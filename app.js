0d5b24712c714f6942c5d17f64e6b6e68ea61771
Dariusz Golunski <darek.golunski@gmail.com>
2022-10-27 13:36:03 +0200

    Add mongoDB database functionality

--- app.js
+++ app.js
@@ -1,24 +1,45 @@
 //jshint esversion:6
-
+require('dotenv').config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 var _ = require("lodash");
+const mongoose = require("mongoose");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
 const app = express();
-const posts = [];
+
 
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+const mongoDbPass = encodeURIComponent(process.env.MONGODBPASS);
+const mongoDbUser = encodeURIComponent(process.env.MONGODBUSER);
+
+mongoose.connect(`mongodb+srv://${mongoDbUser}:${mongoDbPass}@cluster0.t6q8qhu.mongodb.net/blogDB`);
+
+const postSchema = {
+  postName: String,
+  postContent: String
+
+}
+
+const Post = new mongoose.model("Post", postSchema);
+
 app.get("/", function(req, res){
-  res.render("home", {homeStartingContent: homeStartingContent, posts: posts });
+  Post.find(function(err, foundPosts){
+    if (err){
+      console.log(err);
+    }else{
+      res.render("home", {homeStartingContent: homeStartingContent, posts: foundPosts });
+    }
+  });
+
 
 });
 
@@ -36,11 +57,11 @@ app.get("/compose", function(req, res){
 
 app.post("/compose", function(req, res) {
 
-  const post = {
-    postTitle: req.body.postTitle,
+  const post = new Post({
+    postName: req.body.postTitle,
     postContent: req.body.postContent
-  };
-  posts.push(post);
+  });
+  post.save();
 
   res.redirect("/compose");
 
@@ -49,16 +70,18 @@ app.post("/compose", function(req, res) {
 app.get("/posts/:post", function(req, res){
 const requestedTitle = _.kebabCase(req.params.post);
 
-  for (let i=0; i<posts.length; i++){
-    const storedTitle = posts[i]["postTitle"];
-    const storedContent = posts[i]["postContent"];
 
-    if (_.kebabCase(storedTitle) === requestedTitle){
-      res.render("post", {storedTitle: storedTitle, storedContent: storedContent });
+ Post.findOne({postName: _.capitalize(requestedTitle.replace("-", " "))}, function(err, foundPost){
+   if (err){
+     console.log(err);
+   }else{
 
-    };
-  };
+   res.render("post", {storedTitle: foundPost.postName, storedContent: foundPost.postContent });
+    }
+ });
 });
+
+
 
 
 app.listen(3000, function() {
